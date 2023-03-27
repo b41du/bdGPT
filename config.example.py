6783493f5fdfd10d98664b5d8f64b0a18d9ed11b
baidu <baiiidu888@gmail.com>
2023-03-27 23:05:23 +0800

    add config example

---
+++ config.example.py
@@ -0,0 +1,18 @@
+gpt_base_url = ''
+gpt_token = ''
+gpt_execute_limit = 4 #in seccond
+gpt_messages_init = [{'role': 'system', 'content': 'You are a helpful assistant.'}]
+gpt_model_tuning = 'gpt-3.5-turbo'
+
+images_default_path = [
+    # 'images/default/default_img_1.jpg',
+    # 'images/default/default_img_2.jpg',
+    # 'images/default/default_img_3.jpg',
+    # 'images/default/default_img_4.jpg'
+]
+
+img_destination_path = "images/uploads/"
+font_path = "font/NotoSansSC_Bold.otf"
+
+keyword_list_path = '/keyword_list/keyword_list.txt'
+keyword_length_limit = 10
